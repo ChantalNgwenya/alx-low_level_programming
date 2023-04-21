fa3206e371b399d8502aa3dda52e102b25c67d19
Chantal Ngwenya <ngwenyachantalnosipho@gmail.com>
2023-04-20 17:06:06 -0700

    C program that prints with the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - is an entry point
+ *
+ * Description: a C program that prints using the printf fuction
+ *
+ * Return: is always 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
