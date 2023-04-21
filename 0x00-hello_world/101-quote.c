a6a52f6e08c9d810399fbf23a2e76d4631c72f7e
Chantal Ngwenya <ngwenyachantalnosipho@gmail.com>
2023-04-20 18:23:23 -0700

    fixe file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *
+ * main - is the entry point
+ *
+ * Description: it prints a quote using the write function
+ *
+ * Return: return an erro 1
+*/
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
