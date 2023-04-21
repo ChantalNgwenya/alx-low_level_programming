8e9de230f8dbb12e590f32f773807453cf2fd7a5
Chantal Ngwenya <ngwenyachantalnosipho@gmail.com>
2023-04-21 16:27:55 -0700

    prints all alphabet letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - is an entry point
+ *
+ * Description: prints all the alphabet letters
+ *
+ * Return: Is always 0 (success)
+*/
+int main(void)
+{
+	char alphabet == 'a';
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		++alphabet;
+	}
+	putchar('\n');
+	return (0);
+}
