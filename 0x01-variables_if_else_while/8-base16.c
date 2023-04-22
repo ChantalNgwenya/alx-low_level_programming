96230c149e300c3d41da8d35b9e5ca0a9df3433a
Chantal Ngwenya <ngwenyachantalnosipho@gmail.com>
2023-04-21 17:53:01 -0700

    prints numbers of base16 in lowercase

---
+++ 0x01-variables_if_else_while/8-base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - is an entry point
+ *
+ * Description: prints numbers of base16 in a lowwercase
+ *
+ * Return: its always a 0 (success)
+*/
+int main(void)
+{
+	int digit = 48;/*48; decimal representation of 0*/
+	while (digit <= 102) /*102; is a decimal representation of f*/
+	{
+		putchar(digit);
+		/*after 9 it will loop until 96; `*/
+		if (digit == 57)
+			digit = digit + 39;
+		++digit;
+	}
+	putchar('\n');
+	return (0);
+}
