f60a6c6d39e466acb73f4f1918baba7ea5781fcc
Chantal Ngwenya <ngwenyachantalnosipho@gmail.com>
2023-04-21 17:22:49 -0700

    prints digits 0-9 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - an entry point
+ *
+ * Description: prints digits 0-9 using putchar, while using the int variable
+ *
+ * Return: is always 0 (success)
+*/
+int main(void)
+{
+	int digit = 0;
+	while (digit <= 9)
+	{
+		putchar(digit + '0'); /*this converts digits to ASCII representation*/
+		++digit;
+	}
+	putchar('\n');
+	return (0);
+}
