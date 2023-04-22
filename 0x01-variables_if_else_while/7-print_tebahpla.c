cc094ed36f38ec616d4b57430ab132ec101f2858
Chantal Ngwenya <ngwenyachantalnosipho@gmail.com>
2023-04-21 17:38:05 -0700

    prints a to z in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - an entry point
+ *
+ * Description: print alphabets a-z in reverse
+ *
+ * Return: is always 0 (success)
+*/
+int main(void)
+{
+	char alphabet = 'z';
+	while (alphabet >= 'a')
+	{
+		putchar(alphabet);
+		--alphabet;
+	}
+	putchar('\n');
+	return (0);
+}
