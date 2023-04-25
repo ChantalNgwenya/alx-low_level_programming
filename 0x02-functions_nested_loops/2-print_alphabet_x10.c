c24bbe08a205c5450d32eca29a772dfe597ed54f
Chantal Ngwenya <ngwenyachantalnosipho@gmail.com>
2023-04-25 15:42:45 -0700

    print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - is entry point
+ *
+ * Description - prints alphabets x10
+ *
+ * Return - always 0
+*/
+void print_alphabet_x10(void)
+{
+	int num;
+	int alph;
+
+	for (num = 0; num <= 10; num++)
+	{
+		for (alph = 97; alph <= 122; alph++)
+		{
+			_putchar(alph);
+		}
+		_putchar('\n');
+	}
+}
