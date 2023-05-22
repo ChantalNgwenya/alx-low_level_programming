e5c4ff5a52e3a6546415e4f8e7da4d1429f2a85c
Chantal Nosipho Ngwenya <ngwenyachantalnosipho@gmail.com>
2023-05-22 10:22:40 -0700

    print file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 /**
- * main - entry point
- *
- * Description: prints file name it was compiled from
- * 
- * Return : 0
+ * main - prints file name it was compiled from
+ * Return :always  0
 */
 int main(void)
 {
